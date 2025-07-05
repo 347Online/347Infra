cc1892d4e7530840b971b4046925e282e615b48d
347Online | Katie Janzen <katiejanzen@347online.me>
2025-07-05 18:16:06 -0500

    WIP

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -1,6 +1,6 @@
 { lib, username, ... }:
 {
-  sops.age.keyFile = lib.mkDefault "/home/${username}/.config/sops/age/keys-infra.txt";
+  sops.age.keyFile = lib.mkDefault "/home/${username}/.config/sops/age/keys.txt";
 
   security.pam = {
     sshAgentAuth.enable = lib.mkDefault true;
