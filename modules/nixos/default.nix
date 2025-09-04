0fb93239ccf18fee9fd35e720c9332696c69ef08
347Online | Katie Janzen <katiejanzen@347online.me>
2025-09-03 20:35:02 -0500

    Nixos: Add "experimental" settings nix-command, flakes
    Add username to trusted-users

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -1,6 +1,12 @@
 { lib, username, ... }:
 {
   sops.age.keyFile = lib.mkDefault "/home/${username}/.config/sops/age/keys.txt";
+  nix = {
+    settings = {
+      experimental-features = lib.mkDefault "nix-command flakes";
+      trusted-users = lib.mkDefault [ username ];
+    };
+  };
 
   security.pam = {
     sshAgentAuth.enable = lib.mkDefault true;
