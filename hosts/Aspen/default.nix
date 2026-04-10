1919ae40f9816bc3dd74b0e75f4c247408e30a10
347Online | Katie Janzen <katiejanzen@347online.me>
2026-04-10 16:36:06 -0500

    Add public keys to Astrid/Aspen Config

--- hosts/Aspen/default.nix
+++ hosts/Aspen/default.nix
@@ -26,6 +26,10 @@
       "minecraft-server"
     ];
 
+  users.users.${username}.openssh.authorizedKeys.keys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIE3E1Sps4U+TZUCA2vBiCVDfx60u1k9dmXwecnyhnQf6"
+  ];
+
   nix.settings.trusted-users = [ username ];
 
   # DO NOT EDIT
--- hosts/Astrid/default.nix
+++ hosts/Astrid/default.nix
@@ -19,6 +19,10 @@
     443
   ];
 
+  users.users.${username}.openssh.authorizedKeys.keys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIE3E1Sps4U+TZUCA2vBiCVDfx60u1k9dmXwecnyhnQf6"
+  ];
+
   nix.settings.trusted-users = [ username ];
 
   # DO NOT EDIT
