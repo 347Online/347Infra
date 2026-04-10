c917f284e079b193de73b4fc4d1e31f8c04b0944
347Online | Katie Janzen <katiejanzen@347online.me>
2026-04-10 16:56:53 -0500

    Use writeShellApplication for free shellchecks

--- flake.nix
+++ flake.nix
@@ -72,14 +72,20 @@
           {
             devShells.default = pkgs.mkShell {
               packages = [
-                (pkgs.writeShellScriptBin "deploy-to" ''
-                  nixos-rebuild-ng switch --build-host aspen --target-host $1 --flake . --use-substitutes --sudo --ask-sudo-password
-                '')
-                (pkgs.writeShellScriptBin "deploy-all" ''
-                  for x in {aspen,astrid,172.233.131.81}; do
-                    nixos-rebuild-ng switch --build-host aspen --target-host "$x" --flake . --use-substitutes --sudo --ask-sudo-password
-                  done
-                '')
+                (pkgs.writeShellApplication {
+                  name = "deploy-to";
+                  text = ''
+                    nixos-rebuild-ng switch --build-host aspen --target-host "$1" --flake . --use-substitutes --sudo --ask-sudo-password
+                  '';
+                })
+                (pkgs.writeShellApplication {
+                  name = "deploy-all";
+                  text = ''
+                    for x in {aspen,astrid,172.233.131.81}; do
+                      nixos-rebuild-ng switch --build-host aspen --target-host "$x" --flake . --use-substitutes --sudo --ask-sudo-password
+                    done
+                  '';
+                })
               ];
               buildInputs = [ pkgs.nixos-rebuild-ng ];
             };
