f7dc5cfe4fac11d891cb876b70903e07fe383f35
347Online | Katie Janzen <katiejanzen@347online.me>
2026-04-13 14:48:24 -0500

    Add a module to serve 347Online.me w/ Nginx

--- hosts/Aeris/default.nix
+++ hosts/Aeris/default.nix
@@ -7,6 +7,7 @@
 {
   imports = [
     ./hardware-configuration.nix
+    ./website.nix
   ];
 
   networking.hostName = "Aeris";
---
+++ hosts/Aeris/website.nix
@@ -0,0 +1,22 @@
+{
+  networking.firewall.allowedTCPPorts = [
+    80
+    443
+  ];
+
+  security.acme = {
+    acceptTerms = true;
+    defaults.email = "katiejanzen@347online.me";
+  };
+
+  services.nginx = {
+    enable = true;
+
+    virtualHosts."347Online.me" = {
+      forceSSL = true;
+      enableACME = true;
+      root = "/var/www/347Online.me";
+    };
+  };
+
+}
