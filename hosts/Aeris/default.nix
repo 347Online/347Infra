143518ccdda10ee5a5c650ea38e0308ec35bda9b
347Online | Katie Janzen <katiejanzen@347online.me>
2026-04-13 14:29:31 -0500

    Define GitHub Actions user

--- hosts/Aeris/default.nix
+++ hosts/Aeris/default.nix
@@ -14,9 +14,22 @@
   networking.useDHCP = false;
   networking.interfaces.eth0.useDHCP = true;
 
-  users.users.${username}.openssh.authorizedKeys.keys = [
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOe2vJeqUHEDAm8PpZ7jLoYhKIEbHz2aMQc650FlbrhP"
-  ];
+  users = {
+    groups.github-actions = { };
+    users = {
+      ${username}.openssh.authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOe2vJeqUHEDAm8PpZ7jLoYhKIEbHz2aMQc650FlbrhP"
+      ];
+
+      github-actions = {
+        group = "github-actions";
+        isNormalUser = true;
+        openssh.authorizedKeys.keys = [
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEA57BeLEFUQO7CLSjkrZzlKSLqgfviHXhMDoz/wJ3GS"
+        ];
+      };
+    };
+  };
 
   environment.systemPackages = with pkgs; [
     inetutils
