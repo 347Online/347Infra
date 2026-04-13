da6878044894e0ceb21f85d55f5e315daa556cb1
347Online | Katie Janzen <katiejanzen@347online.me>
2026-04-13 15:37:51 -0500

    Support additional subdomains

--- hosts/Aeris/website.nix
+++ hosts/Aeris/website.nix
@@ -5,6 +5,7 @@
   ];
 
   security.acme = {
+    certs."347Online.me".extraDomainNames = [ "www.347Online.me" ];
     acceptTerms = true;
     defaults.email = "katiejanzen@347online.me";
   };
@@ -17,11 +18,29 @@
     recommendedProxySettings = true;
     recommendedTlsSettings = true;
 
-    virtualHosts."347Online.me" = {
-      forceSSL = true;
-      enableACME = true;
-      root = "/var/www/347Online.me";
+    virtualHosts = {
+      "347Online.me" = {
+        forceSSL = true;
+        enableACME = true;
+        root = "/var/www/347Online.me";
+      };
+
+      "www.347Online.me" = {
+        forceSSL = true;
+        useACMEHost = "347Online.me";
+        globalRedirect = "347Online.me";
+      };
+
+      "brain.347Online.me" = {
+        forceSSL = true;
+        enableACME = true;
+      };
+
+      "blog.347Online.me" = {
+        forceSSL = true;
+        enableACME = true;
+        globalRedirect = "347Online.me/blog";
+      };
     };
   };
-
 }
