a460a5296121002ad81f69472f49d7e97eb9058e
347Online | Katie Janzen <katiejanzen@347online.me>
2026-04-26 15:13:44 -0500

    Do not expose nixos-rebuild-ng in devShell, hide behind script only

--- modules/devShells.nix
+++ modules/devShells.nix
@@ -1,25 +1,28 @@
 {
   perSystem =
-    { pkgs, ... }:
+    { pkgs, lib, ... }:
     {
       devShells.default = pkgs.mkShell {
-        buildInputs = [ pkgs.nixos-rebuild-ng ];
-        packages = [
-          (pkgs.writeShellApplication {
-            name = "deploy-to";
-            text = ''
-              nixos-rebuild-ng switch --build-host aspen --target-host "$1" --flake . --use-substitutes --sudo --ask-sudo-password
-            '';
-          })
-          (pkgs.writeShellApplication {
-            name = "deploy-all";
-            text = ''
-              for x in {aspen,astrid,347online.me}; do
-                nixos-rebuild-ng switch --build-host aspen --target-host "$x" --flake . --use-substitutes --sudo --ask-sudo-password
-              done
-            '';
-          })
-        ];
+        packages =
+          let
+            nixos-rebuild-ng = lib.getExe pkgs.nixos-rebuild-ng;
+          in
+          [
+            (pkgs.writeShellApplication {
+              name = "deploy-to";
+              text = ''
+                ${nixos-rebuild-ng} switch --build-host aspen --target-host "$1" --flake . --use-substitutes --sudo --ask-sudo-password
+              '';
+            })
+            (pkgs.writeShellApplication {
+              name = "deploy-all";
+              text = ''
+                for x in {aspen,astrid,347online.me}; do
+                  ${nixos-rebuild-ng} switch --build-host aspen --target-host "$x" --flake . --use-substitutes --sudo --ask-sudo-password
+                done
+              '';
+            })
+          ];
       };
     };
 }
